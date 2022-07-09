458ab62299a116eb5ecd31f4cf5934b95eab3103
SuryaNuthalapati <suryanuthalapati11@gmail.com>
2022-07-09 13:26:55 -0600

    worked on logout module

--- src/services/AdminService.js
+++ src/services/AdminService.js
@@ -4,14 +4,14 @@ class AdminService {
   createAdmin(adminData) {
     return http.post(`/auth/signup`, adminData, {
       headers: {
-        'x-access-token': sessionStorage.getItem("token")
+        'x-access-token': sessionStorage.getItem("authToken")
       }
     });
   }
   getAllAdmins() {
     return http.get("/superadmin/alladmins", {
       headers: {
-        'x-access-token': sessionStorage.getItem("token")
+        'x-access-token': sessionStorage.getItem("authToken")
       }
     });
   }
